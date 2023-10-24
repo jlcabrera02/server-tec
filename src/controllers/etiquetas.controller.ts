adf58af68ad321157fc487c3941bdcd7464cf966
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2023-10-23 20:43:18 -0600

    Obtención, eliminación y actualización de etiqueta

--- src/controllers/etiquetas.controller.ts
+++ src/controllers/etiquetas.controller.ts
@@ -1,7 +1,31 @@
-import { crearEtiqueta } from '@services/Etiquetas.services';
+import {
+  crearEtiqueta,
+  editarEtiqueta,
+  eliminarEtiqueta,
+  obtenerEtiquetas
+} from '@services/Etiquetas.services';
 
 const controller = {
-  crearEtiqueta: null //
+  obtenerEtiquetas: null, //
+  crearEtiqueta: null, //
+  actualizarEtiqueta: null, //
+  eliminarEtiqueta: null //
+};
+
+controller.obtenerEtiquetas = async (req, res) => {
+  try {
+    const response = await obtenerEtiquetas();
+
+    res.status(200).json({ success: true, response });
+  } catch (err) {
+    console.log(err);
+
+    res.status(400).json({
+      success: false,
+      response: err,
+      msg: 'Error al obtener etiquetas'
+    });
+  }
 };
 
 controller.crearEtiqueta = async (req, res) => {
@@ -18,6 +42,37 @@ controller.crearEtiqueta = async (req, res) => {
       success: false,
       response: err,
       msg: 'Error al ingresar una nueva etiqueta'
+    });
+  }
+};
+
+controller.actualizarEtiqueta = async (req, res) => {
+  try {
+    const { etiqueta } = req.body;
+    const { idetiqueta } = req.params;
+    const response = await editarEtiqueta({ etiqueta, idEtiqueta: idetiqueta });
+    res.status(200).json({ success: true, response });
+  } catch (err) {
+    console.log(err);
+
+    res.status(400).json({
+      success: false,
+      response: err,
+      msg: 'Error al actualizar etiqueta'
+    });
+  }
+};
+
+controller.eliminarEtiqueta = async (req, res) => {
+  try {
+    const { idetiqueta } = req.params;
+    const response = await eliminarEtiqueta({ idEtiqueta: idetiqueta });
+    res.status(200).json({ success: true, response });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: err,
+      msg: 'Error al eliminar etiqueta'
     });
   }
 };
--- src/routes/etiquetas.route.ts
+++ src/routes/etiquetas.route.ts
@@ -3,5 +3,8 @@ import etiquetas from '@controllers/etiquetas.controller';
 
 const router = Router();
 
+router.get('/obtener', etiquetas.obtenerEtiquetas);
 router.post('/insertar', etiquetas.crearEtiqueta);
+router.put('/actualizarxidetiqueta/:idetiqueta', etiquetas.actualizarEtiqueta);
+router.delete('/eliminarxidetiqueta/:idetiqueta', etiquetas.eliminarEtiqueta);
 export default router;
