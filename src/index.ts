9a626238450c94b89995bf4d9e1a6b92736dcf93
blanco <dehara14@gmail.com>
2024-02-25 12:05:24 -0600

    añadido deploy con ghpages

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import { config } from 'dotenv';
 import indexRoute from '@routes/index.route';
-
+//import { createTables } from '@models/index';
+//createTables();
 config();
 
 const app = express();
